ce96161d1c54ee9c33ec2156c0a848d2498acad3
ankush <ankushk385@gmail.com>
2021-04-30 13:46:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import _ from 'lodash';
+import "./App.css"
+
+const fruits = ["apple","banana","orange","peach","cherry","tomato"];
 
 function App() {
+
+  const [filteredArray, setFilteredArray] = useState([])
+  const [input, setInput] = useState("")
+  const filterFun1 = ()=>{
+    const arr = _.difference(fruits,[input])
+    
+    setFilteredArray(arr)
+  }
+  const filterFun2 = ()=>{
+    
+    const arr = _.concat(fruits,[input]);
+    console.log(arr)
+    
+    setFilteredArray(arr)
+  }
+  const filterFun3 = ()=>{
+
+    const arrData = [...fruits]
+    console.log(arrData)
+    const arr = _.fill(arrData,[input]);
+   
+    setFilteredArray(arr)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="array-list">
+      <h2>list of fruits</h2>
+      <ul className="list">
+      {
+        fruits.map((fruit)=>(
+          <li>{fruit}</li>
+        ))
+      }
+      </ul>
+      <input type="text" value={input} onChange={(e)=>setInput(e.target.value)}/>
+      <div className="buttons">
+      <button disabled={!input} onClick={filterFun1}>Filter Value</button>
+      <button disabled={!input} onClick={filterFun2}>Add Value</button>
+      <button disabled={!input} onClick={filterFun3}>Fill Array</button>
+      </div>
+      
+      </div>
+      
+      <div className="filtered-array">
+     {filteredArray=="" ? (<h1 style={{textAlign:"center"}}>Nothing to display</h1>) : (
+       <>
+       <h3 style={{textAlign:"center"}}>filtered array</h3>
+       <ul className="list">
+         {
+          !filteredArray ? (
+            <li></li>
+          ) : (
+           filteredArray.map((el,index)=>(
+             <li key={index}>{el}</li>
+           ))
+          )
+          
+         }
+       </ul>
+       </>
+     )} 
+      </div>
     </div>
   );
 }
